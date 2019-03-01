60bf4cb8edd322a9ec92f90a5a13f1c6387427d0
Chen Peng <pchen@tritonchina.com>
2019-03-01 08:34:23 +0800

    add comments for DomainObjectResolve

--- src/main/java/com/pointcx/spring/mustache/security/DomainObjectResolver.java
+++ src/main/java/com/pointcx/spring/mustache/security/DomainObjectResolver.java
@@ -4,6 +4,11 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.NoSuchElementException;
 
+/**
+ * Back to spring side, we need to resolve the value for parameters in the expression, for example, 'this'
+ * Mustache provided VariableFetcher to do this. so, we need to go back to mustache side, use the VariableFetcher to resolve these variables.
+ * This interface used for this purpose.
+ */
 public interface DomainObjectResolver {
     default Object resolve(ApplicationContext applicationContext, Object mustacheContext, String express)throws NoSuchElementException {
         throw new NoSuchElementException(express);
